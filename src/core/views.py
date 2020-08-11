47508993ff7085e1f9895fe9d0efc21c2fa8e714
marcos <marcoslemeborbafilho@gmail.com>
2020-08-11 16:56:27 -0300

    commit

--- src/core/views.py
+++ src/core/views.py
@@ -1,8 +1,24 @@
 from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
 
+# Third Part imports
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+class TestView(APIView):
+    def get(self, request, *args, **kwargs):
+        data = {"name": "john", "age": 23}
+        return Response(data)
+
+
+
+
 # Create your views here.
-def test_view(request):
+
+"""def test_view(request):
     data = {"name": "john", "age": 23}
 
-    return JsonResponse(data, safe=False)
+    return JsonResponse(data)
+
+    """
--- src/drf_api/urls.py
+++ src/drf_api/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from core.views import test_view
-
+from django.urls import path, include
+from core.views import TestView
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',test_view, name='test')
+    path(r"^api-auth/", include("rest_framework.urls")),
+    path("admin/", admin.site.urls),
+    path("", TestView.as_view(), name="test"),
 ]
+
